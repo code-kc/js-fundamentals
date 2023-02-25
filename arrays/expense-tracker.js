e231b4c6a3b1503995b3d65eaf4d54da64ffe795
Avinash K C <avi.ec24@gmail.com>
2023-02-25 23:36:31 +0530

    apply arrays

---
+++ arrays/expense-tracker.js
@@ -0,0 +1,34 @@
+const account = {
+    name: 'Avinash KC',
+    expenses: [],
+    income: [],
+    addExpense: function (description, amount) {
+        this.expenses.push({
+            description: description,
+            amount: amount
+        })
+    },
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
+    getAccountSummary: function () {
+        let totalExpenses = 0
+        this.expenses.forEach(function (expense) {
+            totalExpenses += expense.amount
+        })
+        let totalIncome = 0
+        this.income.forEach(function (income) {
+            totalIncome += income.amount
+        })
+
+        return `${this.name} has a balance of $${totalIncome - totalExpenses}. $${totalIncome} in income. $${totalExpenses} in expenses.`
+    }
+}
+
+account.addExpense('Rent', 950)
+account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
+console.log(account.getAccountSummary())
